fbe627290dc928b2cd7dd51a7df0af488f59406c
David Miranda <david@storylog.com>
2015-06-03 14:58:11 -0400

    added error detection and session package

--- client/environment-template-helpers.js
+++ client/environment-template-helpers.js
@@ -1,6 +1,10 @@
 Meteor.call('isProductionEnvironment', function (error, isProd) {
-  Session.set('isDevEnvironment', !isProd);
-  Session.set('isProdEnvironment', isProd);
+  if (error) {
+    throw new Meteor.Error('detect-environment', 'Could not determine if in a production environment or not.');
+  } else {
+    Session.set('isDevEnvironment', !isProd);
+    Session.set('isProdEnvironment', isProd);
+  }
 });
 
 Template.registerHelper('isDevEnvironment', function (template) {
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'panphora:environment-template-helpers',
-  version: '0.0.2',
+  version: '0.0.4',
   summary: 'Use template helpers to determine if you are in development or production',
   git: 'https://github.com/panphora/environment-template-helpers',
   documentation: 'README.md'
@@ -9,6 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
   api.use('templating', 'client');
+  api.use('session', 'client');
   api.addFiles('server/environment-template-helpers.js', 'server');
   api.addFiles('client/environment-template-helpers.js', 'client');
 });
